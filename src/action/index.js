f935e005fd9b41407280856a733a2e7cab3384d2
Divyesh304 <savaliyadivyesh304@gmail.com>
2022-08-30 19:15:09 -0700

    firs

---
+++ src/action/index.js
@@ -0,0 +1,12 @@
+
+export const increasNum =()=>{
+    return{
+        type:"INCR"
+    }
+}
+
+export const decreasNum =()=>{
+    return{
+        type:"DECR"
+    }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import updown from './updown'
+import { combineReducers } from 'redux'
+
+const rootreducer = combineReducers({
+  updown
+})
+
+export default rootreducer;
